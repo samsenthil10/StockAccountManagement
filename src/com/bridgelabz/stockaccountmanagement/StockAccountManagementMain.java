db093b328a0d10a4b13a6400444ad64ebf57d95e
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-06 04:55:46 +0530

    Added: Console to enter details of each stock

---
+++ src/com/bridgelabz/stockaccountmanagement/StockAccountManagementMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.stockaccountmanagement;
+
+import com.bridgelabz.datastructures.LinkedList;
+
+public class StockAccountManagementMain {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Stock> stocksList = StockPortfolio.addStocks();
+	}
+}
